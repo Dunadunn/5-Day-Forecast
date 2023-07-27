f51abd1f4e8373da4f3b40c5b3a6018528a7a1e2
Dunadunn <alxrodrig7@gmail.com>
2023-07-26 18:12:00 -0700

    both css and javascript have been placed into an assets folder. Css has been modified to represent a summer theme for the weather and has been optimized for multiple screen sizes assuming that mobile users will require a responsive page. Javascript has been modified to provide functionality to the webpage. Localstorage has been enabled to save the user's recent searches and the dates for the forecast have been converted from Kelvin to Fahrenheit as well as converting the date strings into weekdays. Additional comments have been saved into the javascript file for any users interested in adding onto the app.

---
+++ assets/script.js
@@ -0,0 +1,116 @@
+const apiKey = 'da9c0a991d2933627af49773149c22f8';
+
+function fetchWeatherData(cityName) {
+    const geocodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+
+    fetch(geocodingUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('City not found');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+            // Second fetch request to get the weather forecast
+            return fetch(forecastUrl);
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Save to search history
+            const history = loadSearchHistory();
+            if (!history.includes(cityName)) {
+                history.push(cityName);
+                saveSearchHistory(history);
+            }
+
+            // Display weather data
+            displayCurrentWeather(data);
+            displayForecast(data);
+
+            // Update the search history display
+            displaySearchHistory();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('City not found. Please enter a valid city name.');
+        });
+}
+
+function getWeekday(dateString) {
+    const date = new Date(dateString);
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return weekdays[date.getDay()];
+}
+
+function kelvinToFahrenheit(kelvin) {
+    return ((kelvin - 273.15) * 9/5) + 32;
+}
+
+function displayCurrentWeather(data) {
+    const currentWeatherSection = document.getElementById('current-weather');
+    const temperatureF = kelvinToFahrenheit(data.list[0].main.temp).toFixed(2);  // Convert to Fahrenheit and round to 2 decimal places
+    currentWeatherSection.innerHTML = `
+        <h3>${data.city.name}, ${data.city.country}</h3>
+        <p>${data.list[0].dt_txt}</p>
+        <img src="http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png">
+        <p>Temperature: ${temperatureF} °F</p>
+        <p>Humidity: ${data.list[0].main.humidity}%</p>
+        <p>Wind Speed: ${data.list[0].wind.speed} m/s</p>
+    `;
+}
+
+function displayForecast(data) {
+    const forecastSection = document.getElementById('forecast');
+    forecastSection.innerHTML = '';  // Clear previous forecast
+
+    for (let i = 0; i < data.list.length; i += 8) {  // Pick one forecast every 24 hours
+        const forecast = data.list[i];
+        const date = forecast.dt_txt;
+        const weekday = getWeekday(date);
+        const temperatureF = kelvinToFahrenheit(forecast.main.temp).toFixed(2);  // Convert to Fahrenheit and round to 2 decimal places
+        const forecastElement = document.createElement('div');
+        forecastElement.innerHTML = `
+            <h4>${weekday}</h4>
+            <img src="http://openweathermap.org/img/w/${forecast.weather[0].icon}.png">
+            <p>Temperature: ${temperatureF} °F</p>
+            <p>Humidity: ${forecast.main.humidity}%</p>
+            <p>Wind Speed: ${forecast.wind.speed} m/s</p>
+        `;
+        forecastSection.appendChild(forecastElement);
+    }
+}
+
+
+
+document.getElementById('search-button').addEventListener('click', function() {
+    const cityName = document.getElementById('search-input').value;
+    fetchWeatherData(cityName);
+});
+
+function saveSearchHistory(history) {
+    localStorage.setItem('searchHistory', JSON.stringify(history));
+}
+
+function loadSearchHistory() {
+    const history = localStorage.getItem('searchHistory');
+    return history ? JSON.parse(history) : [];
+}
+
+function displaySearchHistory() {
+    const history = loadSearchHistory();
+    const historyDiv = document.getElementById('search-history');
+    historyDiv.innerHTML = '';  // Clear previous search history
+
+    for (const city of history) {
+        const button = document.createElement('button');
+        button.textContent = city;
+        button.addEventListener('click', function() {
+            fetchWeatherData(city);
+        });
+        historyDiv.appendChild(button);
+    }
+}
